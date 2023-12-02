cb7c4546e7c918422ad2ab1baeeaa975c8816303
André Faria <andfar10@gmail.com>
2023-12-02 15:38:03 +0000

    🌞 Day 2

---
+++ Day2/day2part1.py
@@ -0,0 +1,38 @@
+﻿games_list = ["Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 367 green; 5 green, 1 red",
+"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"]
+
+lines = []
+with open('Day2/input1.txt', 'r') as file:
+    lines = file.read().splitlines()
+
+red_cubes = 12
+blue_cubes = 14
+green_cubes = 13
+color_dict = {"red": red_cubes, "blue": blue_cubes, "green": green_cubes}
+id_valids = []
+def validate_game(game):
+    tmp = game.replace("Game ", "")
+    id_game, game_vals = tmp.split(":")
+    rounds = game_vals.split(";")
+    invalid = False
+    for round in rounds:
+        colors = round.split(",")
+        for color in colors:
+            numb_color = color.strip().split(" ")
+            if(numb_color[1] in color and int(numb_color[0]) > color_dict[numb_color[1]]):
+                invalid = True
+                break
+            
+    if(not invalid):
+       id_valids.append(id_game)
+
+a = [validate_game(game) for game in lines]
+result = sum(map(int, id_valids))
+print(result)
+
+
+
+
---
+++ Day2/day2part2.py
@@ -0,0 +1,32 @@
+﻿import math
+games_list = ["Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green","Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"]
+
+lines = []
+with open('Day2/input2.txt', 'r') as file:
+    lines = file.read().splitlines()
+
+power_game = []
+def validate_game(game):
+    tmp = game.replace("Game ", "")
+    id_game, game_vals = tmp.split(":")
+    rounds = game_vals.split(";")
+    min_dict = {"red": 0, "blue": 0, "green": 0}
+    for round in rounds:
+        colors = round.split(",")
+        for color in colors:
+            numb_color = color.strip().split(" ")
+            if(int(numb_color[0]) > min_dict[numb_color[1]]):
+                min_dict[numb_color[1]] = int(numb_color[0])
+    power_game.append(math.prod(min_dict.values()))
+   
+
+a = [validate_game(game) for game in lines]
+result = sum(map(int, power_game))
+print(result)
+
+
+
+
