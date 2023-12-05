e741349cde930ab38385b047ab0084401e29ae64
André Faria <andfar10@gmail.com>
2023-12-05 20:31:57 +0000

    😈 Day 5

--- Day5/day5part1.py
+++ Day5/day5part1.py
@@ -2,12 +2,17 @@
 with open('Day5/input.txt', 'r') as file:
     lines = file.read()
 seeds, *blocks = lines.split('\n\n')
-
 seeds = list(map(int, seeds.split(":")[1].split()))
-for block in blocks:
-    ranges = []
-    for line in block.splitlines()[1:]:
-        ranges.append(list(map(int, line.split())))
+
+def check_block(blocks):
+    global seeds
+    for block in blocks:
+        ranges = []
+        for line in block.splitlines()[1:]:
+            ranges.append(list(map(int, line.split())))
+        check_seeds(ranges)
+def check_seeds(ranges):
+    global seeds
     new_seeds = []
     for seed in seeds:
         for dst,src,rg in ranges:
@@ -17,4 +22,6 @@ for block in blocks:
         else:
             new_seeds.append(seed)
     seeds = new_seeds
+
+check_block(blocks)
 print(min(seeds))
